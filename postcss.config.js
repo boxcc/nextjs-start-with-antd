b599b38120d0b750693ce4af15c9fc47881a9e3a
boxcc <bob.zhuang@kiwifi.cn>
2022-08-10 16:18:25 +0800

    feat: 开启CSS体积优化

---
+++ postcss.config.js
@@ -0,0 +1,38 @@
+const glob = require('glob');
+
+module.exports = {
+  plugins: [
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false,
+        },
+      },
+    ],
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: [
+          './src/pages/**/*.{js,jsx,ts,tsx}',
+          './src/components/**/*.{js,jsx,ts,tsx}',
+          './src/containers/**/*.{js,jsx,ts,tsx}',
+          ...glob.sync('node_modules/antd/es/**/*.css', { noDir: true }),
+        ],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: ['html', 'body'],
+        // extractors: [
+        //   {
+        //     extractor: (content) => content.match(/([a-zA-Z-]+)(?= {)/g) || [],
+        //     extensions: ['css'],
+        //   },
+        // ],
+      },
+    ],
+  ],
+};
