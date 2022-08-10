3b37ee9600a1a7ab342294318156f6d6bc7804b6
boxcc <bob.zhuang@kiwifi.cn>
2022-08-10 16:25:52 +0800

    fix: 简化next.config的配置方式

--- next.config.js
+++ next.config.js
@@ -3,46 +3,61 @@ const withAntdLess = require('next-plugin-antd-less');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
+const withPlugins = require('next-compose-plugins');
 
-module.exports = withBundleAnalyzer(
-  withAntdLess({
-    // optional
-    // modifyVars: { '@primary-color': '#04f' },
-    // optional
-    lessVarsFilePath: './src/styles/custom.less',
-    // optional
-    lessVarsFilePathAppendToEndOfContent: true,
-    // optional https://github.com/webpack-contrib/css-loader#object
-    cssLoaderOptions: {
-      esModule: false,
-      sourceMap: false,
-      modules: {
-        mode: 'local',
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      use: [
+        {
+          loader: 'babel-loader',
+        },
+        {
+          loader: '@svgr/webpack',
+          options: {
+            babel: false,
+            icon: true,
+          },
+        },
+      ],
+    });
+
+    // config.plugins.push(new Hsuc(Object.assign(HsucOptions, options)));
+
+    return config;
+  },
+
+  swcMinify: true,
+
+  poweredByHeader: false,
+};
+
+module.exports = withPlugins(
+  [
+    withBundleAnalyzer,
+    [
+      withAntdLess,
+      {
+        // optional
+        // modifyVars: { '@primary-color': '#04f' },
+        // optional
+        lessVarsFilePath: './src/styles/custom.less',
+        // optional
+        lessVarsFilePathAppendToEndOfContent: true,
+        // optional https://github.com/webpack-contrib/css-loader#object
+        cssLoaderOptions: {
+          esModule: false,
+          sourceMap: false,
+          modules: {
+            mode: 'local',
+          },
+        },
       },
-    },
-
-    // Other Config Here...
-
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'babel-loader',
-          },
-          {
-            loader: '@svgr/webpack',
-            options: {
-              babel: false,
-              icon: true,
-            },
-          },
-        ],
-      });
-
-      return config;
-    },
-
-    swcMinify: true,
-  }),
+    ],
+  ],
+  nextConfig,
 );
